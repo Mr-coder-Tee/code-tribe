70941deb0bc175e8c55a11b83e8d69aa401edff6
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-07-22 05:43:49 -0800

    chaned

--- javascript fandamentals/script.js
+++ javascript fandamentals/script.js
@@ -1,3 +1,3 @@
 let __age=Number(prompt("Enter your age"));
 let __name=Number(prompt("Enter your name"));
-let __gender=Number(prompt("Enter your gender"));
+let __gender=Number(prompt("Enter your gender"))

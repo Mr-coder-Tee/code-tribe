bae3a8500d02283ed21ccbdab9adf12f8bfe8918
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-07-30 10:07:44 -0800

    react changes

--- react-practice-app/src/Components/counter.jsx
+++ react-practice-app/src/Components/counter.jsx
@@ -2,30 +2,42 @@ import React, { Component } from 'react';
 //1:16:48
 
 class Counter extends Component {
-    state = {  
-        count:0,
-        tags:['tag1','tag2','tag3']
-    };
+    // state = {  
+    //     value:this.props.counter.value,
+    //     tags:['tag1','tag2','tag3']
+    // };
 
-    renderTags(){
-        if(this.state.tags.length==0)return <p>There are no tags</p>;
-        return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>
-    }
+    // renderTags(){
+    //     if(this.state.tags.length===0)return <p>There are no tags</p>;
+    //     return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>
+    // }
     // constructor(){
     //     super();
     //     this.handleIncrement=this.handleIncrement.bind(this);
     // }
 
-    handleIncrement=(product)=>{
-        this.setState({count:this.state.count+1});
-    };
+    // handleIncrement=(product)=>{
+    //     this.setState({value:this.state.value+1});
+    // };
 
     render() { 
         //let classes = this.getBadgeClasses();
+        
         return (
             <div>
+               
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={()=>this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Increment</button>
+                <button 
+                    onClick={()=>this.props.onIncrement(this.props.counter)} 
+                    className="btn btn-secondary btn-sm">
+                    Increment
+                </button>
+                
+                <button 
+                    onClick={()=>this.props.onDelete(this.props.counter.id)} 
+                    className="btn btn-danger btn-sm m-2"> 
+                    Delete
+                </button>
             </div>
         );
     }
@@ -34,13 +46,13 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.props.counter.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const {count}=this.state
-        return count===0?'Zero':count;
+        const {value}=this.props.counter;
+        return value===0?'Zero':value;
     }
 }
  

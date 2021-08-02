3f9182d79243ab0d4f0935432111a6106f8308cc
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-08-01 22:50:04 -0800

    next commit

--- react-app-two/src/components/Profile.jsx
+++ react-app-two/src/components/Profile.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Profile extends Component {
     state = {  }
     render() { 
-        return ( <div>profile</div> );
+        return ( <div>
+                <p><span>Contact no# : 0766166822</span></p>
+                <p><span>Email : tebatsoterrence.manaka@gmail</span></p>
+            </div> );
     }
 }
  
